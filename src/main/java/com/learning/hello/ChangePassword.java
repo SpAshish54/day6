fb907bbf7ad8d84743f446a5c223768f092fad9a
SpAshish54 <102243591+SpAshish54@users.noreply.github.com>
2023-08-11 06:06:33 +0530

    Add files via upload

---
+++ src/main/java/com/learning/hello/ChangePassword.java
@@ -0,0 +1,46 @@
+package com.learning.hello;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Servlet implementation class ChangePassword
+ */
+public class ChangePassword extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String password = request.getParameter("password");
+		PrintWriter out = response.getWriter();
+		
+		String Path = "\\\\wsl.localhost\\Debian\\home\\ashish\\eclipse-workspace\\learning-web\\src\\main\\java\\com\\learning\\hello";
+		BufferedReader br = new BufferedReader(new FileReader(Path));
+		
+		String line;
+		boolean change = false;
+		out.println("<html> <body>");
+		while ((line = br.readLine())!= null) {
+			if(line.equals(password)) {
+				change = true;
+			}
+		}
+		if(change == false) {
+			out.println("Entered password is correct");
+		}
+		else {
+			out.println("Re-enter the password, the password which you entered is blocked");
+		}
+		out.println("</body> </html>");
+	}
+
+}
